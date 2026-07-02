d01668cb79ad8fce446cadf7c48634efb69dcbfa
tasbihaanwaradil <tasbihaanwaradil@gmail.com>
2026-07-02 21:32:35 +0500

    Added Start Button, Timer, Show Question and Options functionality

--- script.js
+++ script.js
@@ -1,75 +1,75 @@
-// const Questions = [
-//     {
-//         question: "Which keyword is used to declare a variable in JavaScript?",
-//         options: ["let", "loop", "define", "int"],
-//         answer: 0,
-//     },
-//     {
-//         question: "Which company developed JavaScript?",
-//         options: ["Google", "Microsoft", "Netscape", "Apple"],
-//         answer: 2,
-//     },
-//     {
-//         question: "Which symbol is used for comments in JavaScript?",
-//         options: ["//", "<!-- -->", "##", "**"],
-//         answer: 0,
-//     },
-//     {
-//         question: "Which method selects an element by ID?",
-//         options: [
-//             "querySelector()",
-//             "getElementById()",
-//             "getElementsByClassName()",
-//             "selectElement()",
-//         ],
-//         answer: 1,
-//     },
-//     {
-//         question: "Which operator checks value and datatype?",
-//         options: ["==", "=", "===", "!="],
-//         answer: 2,
-//     },
-//     {
-//         question: "Which method prints output in the browser console?",
-//         options: [
-//             "console.log()",
-//             "print()",
-//             "document.write()",
-//             "alert()",
-//         ],
-//         answer: 0,
-//     },
-//     {
-//         question: "Which loop executes at least one time?",
-//         options: ["for", "while", "do...while", "foreach"],
-//         answer: 2,
-//     },
-//     {
-//         question: "JavaScript is a ______ language.",
-//         options: [
-//             "Programming",
-//             "Database",
-//             "Operating System",
-//             "Browser",
-//         ],
-//         answer: 0,
-//     },
-//     {
-//         question: "Which event occurs when a button is clicked?",
-//         options: ["mouseover", "click", "change", "submit"],
-//         answer: 1,
-//     },
-//     {
-//         question: "Which method converts JSON into an object?",
-//         options: [
-//             "JSON.stringify()",
-//             "JSON.parse()",
-//             "JSON.convert()",
-//             "JSON.object()",
-//         ],
-//         answer: 1,
-//     },
-// ];
+const Questions = [
+    {
+        question: "Which keyword is used to declare a variable in JavaScript?",
+        options: ["let", "loop", "define", "int"],
+        answer: 0,
+    },
+    {
+        question: "Which company developed JavaScript?",
+        options: ["Google", "Microsoft", "Netscape", "Apple"],
+        answer: 2,
+    },
+    {
+        question: "Which symbol is used for comments in JavaScript?",
+        options: ["//", "<!-- -->", "##", "**"],
+        answer: 0,
+    },
+    {
+        question: "Which method selects an element by ID?",
+        options: [
+            "querySelector()",
+            "getElementById()",
+            "getElementsByClassName()",
+            "selectElement()",
+        ],
+        answer: 1,
+    },
+    {
+        question: "Which operator checks value and datatype?",
+        options: ["==", "=", "===", "!="],
+        answer: 2,
+    },
+    {
+        question: "Which method prints output in the browser console?",
+        options: [
+            "console.log()",
+            "print()",
+            "document.write()",
+            "alert()",
+        ],
+        answer: 0,
+    },
+    {
+        question: "Which loop executes at least one time?",
+        options: ["for", "while", "do...while", "foreach"],
+        answer: 2,
+    },
+    {
+        question: "JavaScript is a ______ language.",
+        options: [
+            "Programming",
+            "Database",
+            "Operating System",
+            "Browser",
+        ],
+        answer: 0,
+    },
+    {
+        question: "Which event occurs when a button is clicked?",
+        options: ["mouseover", "click", "change", "submit"],
+        answer: 1,
+    },
+    {
+        question: "Which method converts JSON into an object?",
+        options: [
+            "JSON.stringify()",
+            "JSON.parse()",
+            "JSON.convert()",
+            "JSON.object()",
+        ],
+        answer: 1,
+    },
+];
 
 
 //Selected Screens
@@ -96,3 +96,88 @@ const wrongAnswers = document.getElementById("wrong")
 const unanswered = document.getElementById("unanswered")
 const percentage = document.getElementById("percentage")
 const review = document.getElementById("review")
+
+//Variables
+let currentQuestion = 0
+let userAnswers = []
+let timeLeft = 60
+let interval
+
+//Start Button Functionality
+startBtn.addEventListener("click", () => {
+    startScreen.classList.add("hidden")
+    quizScreen.classList.remove("hidden")
+
+    showQuestion()
+})
+
+const showQuestion = () => {
+    clearInterval(interval)
+    timeLeft = 60
+
+    startTimer()
+
+    //show questions
+    const q = Questions[currentQuestion]
+
+    //show question no
+    questionNumber.innerText = `Question ${currentQuestion + 1}`
+
+    //show question
+    question.innerText = q.question
+
+
+    options.innerHTML = " " //first empty the options innerHTML then append child in it
+    //show options
+    q.options.map((option, index) => {
+
+        //create option buttons inside options div
+        const btn = document.createElement("button")
+        btn.innerText = option
+
+        //apply styling to button
+        btn.classList.add("option")         //option buttons will not show here we need to append child first
+
+        if (userAnswers[currentQuestion] === index) {
+            btn.classList.add("selected")
+        }
+
+        //user click on button it will store index value of selected option 
+        btn.onclick = () => selectAnswer(index)
+
+        //Append "btn" child to parent "options" it will show options
+        options.appendChild(btn)
+
+
+    })
+
+}
+
+const startTimer = () => {
+    timer.innerText = timeLeft
+
+    interval = setInterval(() => {
+        timeLeft--
+        timer.innerText = timeLeft
+
+        if (timeLeft === 0) {
+            clearInterval(interval)
+            nextQuestion()
+        }
+    }, 1000)
+}
+
+let nextQuestion = () => {
+    if (currentQuestion < Questions.length - 1) {
+        currentQuestion++
+
+        showQuestion()
+    }
+
+}
+
+const selectAnswer = (index) => {
+    userAnswers[currentQuestion] = index
+    showQuestion()
+}
+
